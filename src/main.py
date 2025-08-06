19b695ff2aaf1e3fa6312f89c4cc005f308be33b
Axel Muñiz Tello <axeltello@Axels-Laptop.local>
2025-08-06 15:22:58 -0700

    New Plots Added

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
-# Import Needed Dependencies: 
 import numpy as np
 import matplotlib.pyplot as plt
 from vehicles import scenario_1_data, scenario_2_data, scenario_3_data, scenario_4_data
 import os
 
-# Creates Directory for Plots:
+# Plot Directory: 
 plot_dir = os.path.join(os.path.dirname(__file__), "plots")
 
-# Start of Script:
 np.random.seed(42)
 
-time = 900 # total number of seconds
+time = 900 # number of time steps
 t_axis = np.arange(time)
 
 dt = 1e-1 # time step difference 
@@ -19,12 +17,12 @@ s_0 = 50.0 # initial space gap (60 meters)
 u_0 = 33.0 # initial lead velocity (33 m/s)
 v_0 = 31.0 # initial following velocity (31 m/s)
 
-true_theta = np.array([0.08, 0.12, 1.5]) 
+true_theta = np.array([0.08, 0.12, 12.0]) 
 
 dv_max = 3.0 # maximum acceleration/deceleration (3 m/s^2)
 
 if __name__ == "__main__":
-
+    # All Posible Cases: 
     """'
     Case 1: Random Walk 
     """
@@ -49,4 +47,3 @@ if __name__ == "__main__":
     """    
     print("----------Case 4: Aggressive Behavior----------")
     scenario_4_data(u_0, v_0, s_0, time, dv_max, dt, true_theta)
-
