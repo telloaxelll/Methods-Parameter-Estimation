9b5742516e8742454cf09693b6827b7e4c62145e
Axel Muñiz Tello <axeltello@Axels-Laptop.local>
2025-09-12 21:23:45 -0700

    added documentation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
+# Import needded modules 
 import numpy as np
 import matplotlib.pyplot as plt
 from vehicles import scenario_1_data, scenario_2_data, scenario_3_data, scenario_4_data
 import os
 
-# Plot Directory: 
+# Directory for simulation plots
 plot_dir = os.path.join(os.path.dirname(__file__), "plots")
 
-np.random.seed(41)
+np.random.seed(41) # seed for reproducibility
 
 time = 900 # number of time steps
 t_axis = np.arange(time)
 
-dt = 1e-1 # time step difference 
+dt = 1e-1 # time step difference differential  
 
 s_0 = 50.0 # initial space gap (60 meters)
 u_0 = 33.0 # initial lead velocity (33 m/s)
@@ -22,28 +23,24 @@ true_theta = np.array([0.08, 0.12, 12.0]) # true theta parameter
 dv_max = 3.0 # maximum acceleration/deceleration (3 m/s^2)
 
 if __name__ == "__main__":
-    # All Posible Cases: 
-    """'
-    Case 1: Random Walk 
-    """
+    
+    # Scenario 1: 
+    # - We simulate two vehicles, where u(t) is the lead vehicle and v(t) is the ego
+    #   vehicle. We model these two cars, with u(t) in the lead on a highway. We will
+    #   test two cases.
+    # - Case 1 | Equilibrium: Vehicles are under constant predetermined speed 
+    # - Case 2 | Nonequilibrium: Vehicles that are under nonconstant determined speed 
+    #            (with added fluxuations in velocities)
     print("----------Case 1: Random Walk----------")
     scenario_1_data("NON-EQ", u_0, v_0, s_0, time, dv_max, dt, true_theta)
     scenario_1_data("EQ", u_0, v_0, s_0, time, dv_max, dt, true_theta)
 
-    """
-    Case 2: Road Curvature
-    """
+    # Scenario 2: 
     print("----------Case 2: Induced Road Curvature----------")
     scenario_2_data(u_0, v_0, s_0, time, dv_max, dt, true_theta)
 
-    """
-    Case 3: Suburban Environment
-    """
     print("----------Case 3: Suburban Environment----------")
     scenario_3_data(u_0, v_0, s_0, time, dv_max, dt, true_theta)
-
-    """
-    Case 4: Aggresive Lead Vehicle
-    """    
+   
     print("----------Case 4: Aggressive Behavior----------")
     scenario_4_data(u_0, v_0, s_0, time, dv_max, dt, true_theta)
